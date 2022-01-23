d93ec6729128aa6b8919abc8bfc24177c23abb4c
Laura Walters <laura.walters1098@yahoo.com>
2022-01-23 22:26:04 +0000

    Setting up local storage functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,39 @@
 //  Input form
 
 // Declare variables
+var inputForm = document.getElementById("inputForm");
+var userInput = document.getElementById("userInput");
+var formSubmitBtn = document.getElementById("formSubmit");
 
-// Check for null value
-
-// Save value to local storage (function defined below)
-
-// Add event listent which will call the fetch function (call not define)
+inputForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Check for null value
+  if (userInput.value === "") {
+    alert("please enter a location");
+  } else {
+    // Save value to local storage (function defined below)
+    console.log(userInput.value);
+    saveToLocal(userInput.value);
+    // Add event listener which will call the fetch function (call not define)
+    // fetchFunction(userInput.value);
+  }
+});
 
 // Local storage
 
-// if local storage = null storageArray = []
-
-// push user input to storageArray
-
-// Stringify storageArray and add to local storage
-
-// Parse the array from local storage and push any new values to it
+function saveToLocal(location) {
+  // Parse local storage array so that new values can be added
+  var storageArray = JSON.parse(localStorage.getItem("searchHistory"));
+  // Checking for empty local storage
+  if (storageArray === null) {
+    var storageArray = [];
+  }
+  // push user input to storageArray
+  storageArray.push(location);
+  console.log(storageArray);
+  // Stringify storageArray and add to local storage
+  localStorage.setItem("searchHistory", JSON.stringify(storageArray));
+}
 
 // Buttons
 
