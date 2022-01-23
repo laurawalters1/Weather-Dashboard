46241f357f77b86de61e5582eb16e9c038d0d558
Laura Walters <laura.walters1098@yahoo.com>
2022-01-23 21:12:43 +0000

    adding file structure

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+//  Input form
+
+// Declare variables
+
+// Check for null value
+
+// Save value to local storage (function defined below)
+
+// Add event listent which will call the fetch function (call not define)
+
+// Local storage
+
+// if local storage = null storageArray = []
+
+// push user input to storageArray
+
+// Stringify storageArray and add to local storage
+
+// Parse the array from local storage and push any new values to it
+
+// Buttons
+
+// Use local storage array to create buttons for each searched location
+
+// Add a value to these buttons (data-attribute) which will be the location name
+
+// Add event listener to the buttons which will call the fetch function (call not define)
+
+// Fetch function
+
+// Takes values from either input or buttons
+
+// Format the data into dashboard structure
