9b9156da83c78f978a1c68b1f83663f95bae7d92
Laura Walters <laura.walters1098@yahoo.com>
2022-01-24 20:23:55 +0000

    creating buttons from local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 var inputForm = document.getElementById("inputForm");
 var userInput = document.getElementById("userInput");
 var formSubmitBtn = document.getElementById("formSubmit");
-
+var searchHistoryList = document.getElementById("searchHistoryList");
 inputForm.addEventListener("submit", function (e) {
   e.preventDefault();
   // Check for null value
@@ -33,16 +33,38 @@ function saveToLocal(location) {
   console.log(storageArray);
   // Stringify storageArray and add to local storage
   localStorage.setItem("searchHistory", JSON.stringify(storageArray));
+  //   searchHistoryButtons(storageArray);
 }
 
 // Buttons
 
 // Use local storage array to create buttons for each searched location
+function searchHistoryButtons(array) {
+  for (var i = 0; i < array.length; i++) {
+    var locationLink = document.createElement("a");
+    locationLink.href = "";
+    var locationButton = document.createElement("button");
+    locationButton.textContent = array[i];
+    locationButton.appendChild(locationLink);
+    locationButton.classList.add("locationButton");
+    var locationListItem = document.createElement("li");
+    locationListItem.appendChild(locationButton);
+    searchHistoryList.appendChild(locationListItem);
+    locationButton.addEventListener("click", function () {});
+  }
+}
 
+searchHistoryButtons(JSON.parse(localStorage.getItem("searchHistory")));
 // Add a value to these buttons (data-attribute) which will be the location name
 
 // Add event listener to the buttons which will call the fetch function (call not define)
+var locationButtonArray = document.querySelectorAll(".locationButton");
 
+for (var i = 0; i < locationButtonArray.length; i++) {
+  locationButtonArray[i].addEventListener("click", function () {
+    console.log(locationButtonArray[i]);
+  });
+}
 // Fetch function
 
 // Takes values from either input or buttons
